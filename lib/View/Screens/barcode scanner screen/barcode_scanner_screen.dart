57cc4efc4973f2d206a4a90a3960856365fd1df7
Mohamed Mahmoud <m.raslan97@gmail.com>
2023-10-25 22:27:52 +0300

    added barcode example, it now scans barcode, but returns no data

--- lib/View/Screens/barcode scanner screen/barcode_scanner_screen.dart
+++ lib/View/Screens/barcode scanner screen/barcode_scanner_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class BarcodeScannerScreen extends StatefulWidget {
   const BarcodeScannerScreen({Key? key}) : super(key: key);
@@ -7,9 +9,21 @@ class BarcodeScannerScreen extends StatefulWidget {
   @override
   State<BarcodeScannerScreen> createState() => _BarcodeScannerScreenState();
 }
-
 class _BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
   String result = '';
+  var _scanBarcode;
+  Future scanBarcodeNormal() async {
+    String barcodeScanRes;
+
+    barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        "#ff6666", "Cancel", true, ScanMode.QR);
+    print(barcodeScanRes);
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,16 +33,18 @@ class _BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
           children: [
             ElevatedButton(
               onPressed: () async {
-                var res = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SimpleBarcodeScannerPage(),
-                    ));
-                setState(() {
-                  if (res is String) {
-                    result = res;
-                  }
-                });
+                var resulttt = await  scanBarcodeNormal();
+                print(resulttt.toString());
+                // var res = await Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //       builder: (context) => const SimpleBarcodeScannerPage(),
+                //     ));
+                // setState(() {
+                //   if (res is String) {
+                //     result = res;
+                //   }
+                // });
               },
               child: const Text('Open Scanner'),
             ),
