4658ffa964547d516e9400946c7dac13188d4c91
Mohamed Mahmoud <m.raslan97@gmail.com>
2023-10-25 23:13:25 +0300

    used a more accurate barcode lookup API

--- lib/View/Screens/barcode scanner screen/barcode_scanner_screen.dart
+++ lib/View/Screens/barcode scanner screen/barcode_scanner_screen.dart
@@ -1,7 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ProductInfo {
+  final String productName;
+  final String companyName;
+  final List<String> keywords;
+  final List<String> allergens;
+  final String countries;
+  final String imageUrl;
+
+  ProductInfo({
+    required this.productName,
+    required this.companyName,
+    required this.keywords,
+    required this.allergens,
+    required this.countries,
+    required this.imageUrl,
+  });
+
+  factory ProductInfo.fromJson(Map<String, dynamic> json) {
+    final productData = json['product'];
+    return ProductInfo(
+      productName: productData['product_name'] ?? '',
+      companyName: productData['brands'] ?? '',
+      keywords: List<String>.from(productData['_keywords'] ?? []),
+      allergens: List<String>.from(productData['allergens_tags'] ?? []),
+      countries: productData['countries'] ?? '',
+      imageUrl: productData['image_url'] ?? '',
+    );
+  }
+}
+
+Future<ProductInfo> getProductInfo(String barcode) async {
+  final response = await http.get(Uri.parse('https://world.openfoodfacts.org/api/v0/product/$barcode.json'));
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+
+    if (jsonResponse.containsKey('product')) {
+      return ProductInfo.fromJson(jsonResponse);
+    }
+  }
+
+  throw Exception('Failed to get product info for barcode $barcode');
+}
 
 class BarcodeScannerScreen extends StatefulWidget {
   const BarcodeScannerScreen({Key? key}) : super(key: key);
@@ -9,15 +53,41 @@ class BarcodeScannerScreen extends StatefulWidget {
   @override
   State<BarcodeScannerScreen> createState() => _BarcodeScannerScreenState();
 }
+
 class _BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
   String result = '';
   var _scanBarcode;
-  Future scanBarcodeNormal() async {
+  late ProductInfo productInfo = ProductInfo(
+    productName: '',
+    companyName: '',
+    keywords: [],
+    allergens: [],
+    countries: '',
+    imageUrl: '',
+  );
+
+  void resetProductInfo(ProductInfo productInfo)
+  {
+    productInfo = ProductInfo(
+      productName: '',
+      companyName: '',
+      keywords: [],
+      allergens: [],
+      countries: '',
+      imageUrl: '',
+    );
+  }
+
+  Future<void> scanBarcodeNormal() async {
+    setState(() {
+      resetProductInfo(productInfo);
+    });
     String barcodeScanRes;
 
     barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
         "#ff6666", "Cancel", true, ScanMode.QR);
-    print(barcodeScanRes);
+
+    productInfo = await getProductInfo(barcodeScanRes);
 
     setState(() {
       _scanBarcode = barcodeScanRes;
@@ -33,22 +103,17 @@ class _BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
           children: [
             ElevatedButton(
               onPressed: () async {
-                var resulttt = await  scanBarcodeNormal();
-                print(resulttt.toString());
-                // var res = await Navigator.push(
-                //     context,
-                //     MaterialPageRoute(
-                //       builder: (context) => const SimpleBarcodeScannerPage(),
-                //     ));
-                // setState(() {
-                //   if (res is String) {
-                //     result = res;
-                //   }
-                // });
+                await scanBarcodeNormal();
               },
               child: const Text('Open Scanner'),
             ),
-            Text('Barcode Result: $result'),
+            Text('Barcode Result: $_scanBarcode'),
+            Text('Product Name: ${productInfo.productName}'),
+            Text('Company Name: ${productInfo.companyName}'),
+            Text('Keywords: ${productInfo.keywords.join(", ")}'),
+            Text('Allergens: ${productInfo.allergens.join(", ")}'),
+            Text('Countries: ${productInfo.countries}'),
+            Text('Image URL: ${productInfo.imageUrl}'),
           ],
         ),
       ),
